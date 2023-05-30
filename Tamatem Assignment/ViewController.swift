a6abb30325f88b747e76b9c5e0ecf5c26b539405
Osama abuhdba <o.abuhadba@dominate.dev>
2023-05-31 00:13:27 +0300

    Initial Commit

---
+++ Tamatem Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamatem Assignment
+//
+//  Created by Osama Abu Hdba on 31/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
